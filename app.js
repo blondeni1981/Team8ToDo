17016ed87ea33ea01e28932eb3d6a8aa4581848c
samcarson <carsonsam4@gmail.com>
2021-10-04 14:03:09 -0700

    Added final pages, including styling

--- app.js
+++ app.js
@@ -4,16 +4,24 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-
-
 // index page
-app.get('/', function(_req, res) {
+app.get('/', function (_req, res) {
   res.render('pages/index');
 });
 
 // nicole page
-app.get('/nicole', function(_req, res) {
+app.get('/nicole', function (_req, res) {
   res.render('pages/nicole');
+});
+
+// Sam page
+app.get('/sam', function (_req, res) {
+  res.render('pages/sam');
+});
+
+// Gaeul's page
+app.get('/gaeul', function (_req, res) {
+  res.render('pages/gaeul');
 });
 
 app.listen(8080);
